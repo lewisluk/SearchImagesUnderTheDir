25c252b94343b6ffb2aaf243663392bad684d081
lewisluk <31683892+lewisluk@users.noreply.github.com>
2019-10-22 16:26:47 +0800

    Add files via upload

---
+++ searchImages.py
@@ -0,0 +1,23 @@
+import os
+
+def get_image_path(path:str, result_path:list)->list:
+    items = os.listdir(path)
+
+    for item in items:
+        current_item_path = os.path.join(path, item)
+        if '压缩后' in current_item_path:
+            continue
+        if item.endswith('jpg') or item.endswith('JPG'):
+            result_path.append(current_item_path)
+        else:
+            if os.path.isdir(current_item_path):
+                print('current dir:'+current_item_path)
+                result_path = get_image_path(current_item_path, result_path)
+
+    return result_path
+
+path = '/media/lewisluk/My Passport'
+with open('images_path.txt', 'w') as f:
+    paths = get_image_path(path, [])
+    for i in paths:
+        f.write(i+'\n')
